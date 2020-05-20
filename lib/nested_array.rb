d6423043860acf68779150a26618c52b3470fd0d
Stephen C Galvan <stephennoahgalvan@gmail.com>
2020-05-20 21:51:36 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -34,6 +34,7 @@ end
 def matrix_lookup(matrix, row, column)
   matrix[column][row] 
   # Given any matrix (array of arrays), a row index and a column index, 
+  matrix 
   # Return the matrix's content at that row and and column
 end
 
